f9d771021f0d56e8786a738afb2909ec0e1c3b2c
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-15 16:47:48 +0300

    added comms for quant package

--- internal/core/quant/quant.go
+++ internal/core/quant/quant.go
@@ -5,12 +5,22 @@ type Quant struct {
 	text  string
 }
 
+// New creates new instance of Quant with given name
+//
+// Pre-cond: given title
+//
+// Post-cond: created new instance of Quant
 func New(title string) Quant {
 	return Quant{
 		title: title,
 	}
 }
 
+// Title returns title of given Quant instance
+//
+// Pre-cond: given instance of Thread
+//
+// Post-cond: return title of Quant
 func Title(q Quant) string {
 	return q.title
 }
