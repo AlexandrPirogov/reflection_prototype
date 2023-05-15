0d46ec9e823410b37911f68d8b4f98bf6d8ef545
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-15 16:52:27 +0300

    added process type

--- internal/core/process/process.go
+++ internal/core/process/process.go
@@ -3,5 +3,27 @@ package process
 import "reflection_prototype/internal/core/thread"
 
 type Process struct {
-	Thread map[string]thread.Thread
+	title   string
+	threads map[string]thread.Thread
+}
+
+// New creates new instance of Process with given name
+//
+// Pre-cond: given title
+//
+// Post-cond: created new instance of Process
+func New(title string) Process {
+	return Process{
+		title:   title,
+		threads: make(map[string]thread.Thread),
+	}
+}
+
+// Title returns title of given Process instance
+//
+// Pre-cond: given instance of Process
+//
+// Post-cond: return title of Process
+func Title(p Process) string {
+	return p.title
 }
---
+++ internal/core/process/process_test.go
@@ -0,0 +1,15 @@
+package process
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateProcessWithCorrectTitle(t *testing.T) {
+	title := "some_title"
+
+	sut := New(title)
+
+	assert.Equal(t, title, sut.title)
+}
