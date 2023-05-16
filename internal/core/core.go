6e251ab1fe922ee831ecb1dd6d85735b3f2106ff
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-16 13:23:28 +0300

    added function for creating process

--- internal/core/core.go
+++ internal/core/core.go
@@ -1,1 +1,13 @@
 package core
+
+import "reflection_prototype/internal/core/process"
+
+// CreateProcess creates new process with given name
+//
+// Pre-cond: given title for new process. Title is unique
+//
+// Post-cond: if title is unique -- returns nil, otherwise returns err
+func CreateProcess(title string) error {
+	_, err := process.New(title)
+	return err
+}
