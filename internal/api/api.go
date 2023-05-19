43538cce7501d8f43ccdf9c7491c47e3b86a76e4
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-19 17:33:43 +0300

    added create and read operations for processes

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,6 +4,7 @@ import "reflection_prototype/internal/core/process"
 
 type Storer interface {
 	StoreProcess(p process.Process) error
+	ReadProcesses(p process.Process) ([]process.Process, error)
 }
 
 type Handler struct {
--- internal/api/process.go
+++ internal/api/process.go
@@ -8,6 +8,7 @@ import (
 	"reflection_prototype/internal/core/process"
 )
 
+// Store process route that accepts json representation of Process and stores it to Storage
 func (h *Handler) StoreProcess(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -32,5 +33,38 @@ func (h *Handler) StoreProcess(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusCreated)
+	w.Write(body)
+}
+
+func (h *Handler) ReadProcesses(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var proc process.Process
+	err = json.Unmarshal(body, &proc)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	processes, err := h.S.ReadProcesses(proc)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err = json.Marshal(processes)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 	w.Write(body)
 }
