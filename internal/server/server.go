bd6b282b50edfadc9442234218c851de7be7662d
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-20 13:10:28 +0300

    added jwt middleware

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,17 @@ package server
 
 import (
 	"net/http"
+	"reflection_prototype/internal/core/auth/jwt"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 type Handler interface {
+	Login(w http.ResponseWriter, r *http.Request)
+	Register(w http.ResponseWriter, r *http.Request)
+
 	StoreProcess(w http.ResponseWriter, r *http.Request)
 	ReadProcesses(w http.ResponseWriter, r *http.Request)
 
@@ -22,14 +27,23 @@ func New(h Handler) *http.Server {
 	r := chi.NewRouter()
 	r.Use(middleware.AllowContentType("application/json"))
 	r.Use(middleware.Logger)
-	r.Post("/list/processes", h.ReadProcesses)
-	r.Post("/processes", h.StoreProcess)
 
-	r.Post("/list/threads", h.ReadThreads)
-	r.Post("/threads", h.StoreThread)
+	r.Post("/login", h.Login)
+	r.Post("/register", h.Register)
 
-	r.Post("/quants", h.StoreQuant)
-	r.Post("/list/quants", h.ReadQuants)
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(jwt.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/list/processes", h.ReadProcesses)
+		r.Post("/processes", h.StoreProcess)
+
+		r.Post("/list/threads", h.ReadThreads)
+		r.Post("/threads", h.StoreThread)
+
+		r.Post("/quants", h.StoreQuant)
+		r.Post("/list/quants", h.ReadQuants)
+
+	})
 	return &http.Server{
 		Addr:    ":8080",
 		Handler: r,
