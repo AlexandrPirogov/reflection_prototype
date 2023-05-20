6efca8b8042f4504766500554c736fccdf207b0d
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-20 10:37:08 +0300

    added methods to interface Storer

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,8 +2,17 @@ package storage
 
 import (
 	"reflection_prototype/internal/core/process"
+	"reflection_prototype/internal/core/quant"
+	"reflection_prototype/internal/core/thread"
 )
 
 type Storer interface {
 	StoreProcess(p process.Process) error
+	ReadProcesses(p process.Process) ([]process.Process, error)
+
+	StoreThread(t thread.Thread) error
+	ReadThreads(t thread.Thread) ([]thread.Thread, error)
+
+	StoreQuant(q quant.Quant) error
+	ReadQuants(q quant.Quant) ([]quant.Quant, error)
 }
