1b7de9adb520ad6c611a3cef90576b83f27db40b
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-20 10:39:58 +0300

    added routes for threads and quants

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,13 +9,27 @@ import (
 
 type Handler interface {
 	StoreProcess(w http.ResponseWriter, r *http.Request)
+	ReadProcesses(w http.ResponseWriter, r *http.Request)
+
+	StoreThread(w http.ResponseWriter, r *http.Request)
+	ReadThreads(w http.ResponseWriter, r *http.Request)
+
+	StoreQuant(w http.ResponseWriter, r *http.Request)
+	ReadQuants(w http.ResponseWriter, r *http.Request)
 }
 
 func New(h Handler) *http.Server {
 	r := chi.NewRouter()
 	r.Use(middleware.AllowContentType("application/json"))
 	r.Use(middleware.Logger)
-	r.Post("/processes/", h.StoreProcess)
+	r.Post("/list/processes", h.ReadProcesses)
+	r.Post("/processes", h.StoreProcess)
+
+	r.Post("/list/threads", h.ReadThreads)
+	r.Post("/threads", h.StoreThread)
+
+	r.Post("/quants", h.StoreQuant)
+	r.Post("/list/quants", h.ReadQuants)
 	return &http.Server{
 		Addr:    ":8080",
 		Handler: r,
