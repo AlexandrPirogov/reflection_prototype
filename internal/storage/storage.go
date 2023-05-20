4b36b3fed9fc1170d9fcfcef4d6e97efb90ea12c
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-20 13:10:01 +0300

    added methods to interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,12 +1,16 @@
 package storage
 
 import (
+	"reflection_prototype/internal/core/auth/user"
 	"reflection_prototype/internal/core/process"
 	"reflection_prototype/internal/core/quant"
 	"reflection_prototype/internal/core/thread"
 )
 
 type Storer interface {
+	Login(u user.User) (string, error)
+	Register(u user.User) error
+
 	StoreProcess(p process.Process) error
 	ReadProcesses(p process.Process) ([]process.Process, error)
 
