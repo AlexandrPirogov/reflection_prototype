442299f4f55ff7328c625397d887cbfc81389e9a
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-22 16:45:36 +0300

    added new method for reading thread. Renamed methods

--- internal/api/thread.go
+++ internal/api/thread.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"reflection_prototype/internal/core/thread"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // Store process route that accepts json representation of Process and stores it to Storage
@@ -36,7 +38,7 @@ func (h *Handler) StoreThread(w http.ResponseWriter, r *http.Request) {
 	w.Write(body)
 }
 
-func (h *Handler) ReadThreads(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) ListThreads(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Println(err)
@@ -52,7 +54,7 @@ func (h *Handler) ReadThreads(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	threads, err := h.S.ReadThread(t)
+	threads, err := h.S.ListThreads()
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -67,4 +69,32 @@ func (h *Handler) ReadThreads(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write(body)
+}
+
+func (h *Handler) ReadThread(w http.ResponseWriter, r *http.Request) {
+	procTitle := chi.URLParam(r, "process")
+	threadTitle := chi.URLParam(r, "thread")
+	thread, err := thread.New(procTitle, threadTitle)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res, err := h.S.ReadThread(thread)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := json.Marshal(res)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(body)
+
 }
