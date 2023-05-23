68ae0f953506fcdedb850c481a8e8e1de0673d49
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-23 17:47:17 +0300

    removed jwt column

--- internal/storage/postgres/auth.go
+++ internal/storage/postgres/auth.go
@@ -8,34 +8,23 @@ import (
 )
 
 func (pg *pgConnection) Login(u user.User) (string, error) {
-	var jwt string
-	query := `select jwt from users
+	var token string
+	var id int
+	query := `select id from users
 	where login = $1 and email = $2 and pwd = $3`
-	err := pg.conn.QueryRow(context.Background(), query, u.Login, u.Email, u.Pwd).Scan(&jwt)
+	err := pg.conn.QueryRow(context.Background(), query, u.Login, u.Email, u.Pwd).Scan(&id)
 	if err != nil {
 		log.Println(err)
-		return jwt, err
+		return token, err
 	}
 
-	return jwt, nil
+	token = jwt.GenerateJWT(u)
+	return token, nil
 }
 
 func (pg *pgConnection) Register(u user.User) error {
-
-	/*
-		ID SERIAL PRIMARY KEY,
-		LOGIN VARCHAR(255),
-		NAME VARCHAR(255),
-		SURNAME VARCHAR(255),
-		EMAIL VARCHAR(300),
-		PWD VARCHAR(300),
-		PHOTO VARCHAR(300),
-		JWT VARCHAR(300),
-		REGISTERED_AT TIMESTAMP
-	*/
-	query := "insert into users values(default, $1, $2, $3, $4, $5, null, $6,now())"
-	jwt := jwt.GenerateJWT(u)
-	_, err := pg.conn.Exec(context.Background(), query, u.Login, u.Name, u.Surname, u.Email, u.Pwd, jwt)
+	query := "insert into users values(default, $1, $2, $3, $4, $5, null, now())"
+	_, err := pg.conn.Exec(context.Background(), query, u.Login, u.Name, u.Surname, u.Email, u.Pwd)
 	if err != nil {
 		log.Println(err)
 		return err
