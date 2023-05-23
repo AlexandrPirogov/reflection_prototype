44dc775c7ff0219d29a05748fb68997e3edfa685
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-05-23 17:46:41 +0300

    added jwt auth

--- internal/core/auth/jwt/jwt.go
+++ internal/core/auth/jwt/jwt.go
@@ -6,11 +6,15 @@ import (
 	"github.com/go-chi/jwtauth/v5"
 )
 
+var claims = make(map[string]interface{}, 0)
 var TokenAuth *jwtauth.JWTAuth = jwtauth.New("HS256", []byte("secret"), nil)
 
 func GenerateJWT(u user.User) string {
+
 	key := u.Login + u.Pwd + u.Email
-	_, tokenString, _ := TokenAuth.Encode(map[string]interface{}{key: u})
+	claims[key] = u
+	//jwtauth.SetExpiry(claims, time.Now().Add(time.Second*30))
+	_, tokenString, _ := TokenAuth.Encode(claims)
 
 	return tokenString
 }
